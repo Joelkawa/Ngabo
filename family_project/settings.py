056f52b9c3f9d2068df3ebafc7090ef04e879d7a
Joel <joel.btech24@gmail.com>
2025-05-15 08:23:29 +0300

    pdw update

--- family_project/settings.py
+++ family_project/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv,
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST'),
         'PORT': 5432
     }
